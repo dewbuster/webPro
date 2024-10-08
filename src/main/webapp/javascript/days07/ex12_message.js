1a002a576587c09bb0918e0428d7847d30fc8ea5
dewbuser <chwon301@gmail.com>
2024-10-08 17:32:16 +0900

    days07-js

---
+++ src/main/webapp/javascript/days07/ex12_message.js
@@ -0,0 +1,11 @@
+/**
+ * 
+ */
+
+const message = ()=>{
+    const name = "홍길동";
+    const age = 20;
+    return `${name} / ${age}`;
+}
+
+export default message;
---
+++ src/main/webapp/javascript/days07/ex12_person.js
@@ -0,0 +1,12 @@
+/**
+ * ex12_person.js
+ 	변수 내보내기
+ */
+ /*
+ export const name = "admin";
+ export const age = 20;
+ */
+
+ const name = "admin";
+ const age = 20;
+ export { name, age };
